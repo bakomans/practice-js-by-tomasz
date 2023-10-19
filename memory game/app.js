3e52e11a579f888e53856efedeee34abdeeef753
bakomans <bakomans@gmail.com>
2023-10-19 10:12:55 +0100

    add function flip card and comments

--- memory game/app.js
+++ memory game/app.js
@@ -1,65 +1,92 @@
+// create an array
+
+
 var cardArray = [
-    {
-        name: "fries",
-        img:"img/fries.png",
-    },
-    {
-        name: "cheeseburger",
-        img:"img/cheeseburger.png",
-    },
-    {
-        name: "hotdog",
-        img:"img/hotdog.png",
-    },
-    {
-        name: "ice-cream",
-        img:"img/ice-cream.png",
-    },
-    {
-        name: "milkshake",
-        img:"img/milkshake.png",
-    },
-    {
-        name: "pizza",
-        img:"img/pizza.png",
-    },
-    {
-        name: "fries",
-        img:"img/fries.png",
-    },
-    {
-        name: "cheeseburger",
-        img:"img/cheeseburger.png",
-    },
-    {
-        name: "hotdog",
-        img:"img/hotdog.png",
-    },
-    {
-        name: "ice-cream",
-        img:"img/ice-cream.png",
-    },
-    {
-        name: "milkshake",
-        img:"img/milkshake.png",
-    },
-    {
-        name: "pizza",
-        img:"img/pizza.png",
-    },
-]
+  {
+    name: "fries",
+    img: "img/fries.png",
+  },
+  {
+    name: "cheeseburger",
+    img: "img/cheeseburger.png",
+  },
+  {
+    name: "hotdog",
+    img: "img/hotdog.png",
+  },
+  {
+    name: "ice-cream",
+    img: "img/ice-cream.png",
+  },
+  {
+    name: "milkshake",
+    img: "img/milkshake.png",
+  },
+  {
+    name: "pizza",
+    img: "img/pizza.png",
+  },
+  {
+    name: "fries",
+    img: "img/fries.png",
+  },
+  {
+    name: "cheeseburger",
+    img: "img/cheeseburger.png",
+  },
+  {
+    name: "hotdog",
+    img: "img/hotdog.png",
+  },
+  {
+    name: "ice-cream",
+    img: "img/ice-cream.png",
+  },
+  {
+    name: "milkshake",
+    img: "img/milkshake.png",
+  },
+  {
+    name: "pizza",
+    img: "img/pizza.png",
+  },
+];
 
 cardArray.sort(() => 0.5 - Math.random());
 
 var gridDisplay = document.querySelector("#grid");
+var cardsChosen = [];
 
-// function create board 
-function createBoard () {
-    for (var i = 0; i < cardArray.length; i++) {
-        var card = document.createElement("img");
-        card.setAttribute("src", "img/blank.png");
-        card.setAttribute("data-id", i);
-        gridDisplay.appendChild(card);
-    }
+// function create board
+function createBoard() {
+  for (var i = 0; i < cardArray.length; i++) {
+    var card = document.createElement("img");
+    card.setAttribute("src", "img/blank.png");
+    card.setAttribute("data-id", i);
+    card.addEventListener("click", flipCard);
+    gridDisplay.appendChild(card);
+  }
 }
-createBoard()
+// call create board
+
+createBoard();
+
+// function check match
+
+function checkMatch() {
+  if (cardsChosen[0] == cardsChosen[1]) {
+    alert("you found the match");
+  }
+}
+
+// flip card function
+
+function flipCard() {
+  var cardId = this.getAttribute("data-id");
+  cardsChosen.push(cardArray[cardId].name);
+  console.log("clicked", cardId);
+  this.setAttribute("src", cardArray[cardId].img);
+  if (cardsChosen.length === 2) {
+    setTimeout(checkMatch, 500);
+  }
+}
