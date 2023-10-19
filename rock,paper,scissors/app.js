31efa3c3c507317273b95166331975bef6459364
bakomans <bakomans@gmail.com>
2023-10-19 08:42:49 +0100

    write function for user and computer choice

--- rock,paper,scissors/app.js
+++ rock,paper,scissors/app.js
@@ -1,7 +1,10 @@
+// main var declaration
+
 var computerChoiceDisplay = document.getElementById("computer-choice");
 var yourChoiceDisplay = document.getElementById("your-choice");
 var resultDisplay = document.getElementById("results");
 var yourChoice
+var computerChoice
 
 // possible choices user
 
@@ -9,6 +12,22 @@ var possibleChoices = document.querySelectorAll("button");
 possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener("click", (e) => {
    yourChoice = e.target.id
    yourChoiceDisplay.innerHTML = yourChoice
+   generateComputerChoice()
 }));
 
-// computer choices
+// computer choices function
+
+function generateComputerChoice() {
+    var randomNumber = Math.floor(Math.random() * 3) +1
+
+    if (randomNumber === 1) {
+        computerChoice = "rock"
+    }
+    if (randomNumber === 2) {
+        computerChoice = "paper"
+    }
+    if (randomNumber === 3) {
+        computerChoice = "scissors"
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
+};
